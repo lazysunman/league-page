c9a634bd0d44b00b60d4a940d89040aec4bf6712
lazysunman <91502638+lazysunman@users.noreply.github.com>
2021-09-30 14:07:36 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "735361814304567296"; // your league ID
 export const leagueName = "The Gentlemen's Coalition"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 150; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
